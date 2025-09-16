e54f43ee9f5a557287f3312ddcfadbf70419d142
Analicesilva-27 <da.silva.analice@escola.pr.gov.br>
2025-09-16 08:23:23 -0300

    entropia

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const numeroSenha = document.querySelector("parametro-senha__texto");
+const numeroSenha = document.querySelector(".parametro-senha__texto");
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
+const letraMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letraMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789'
+const simbolos = '!@%*?';
 
-const botoes = document.querySelectorAll('parametro-senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const CampoSenha = document.querySelector("#campo-senha");
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentoTamanho;
@@ -25,12 +32,10 @@ function aumentoTamanhoTamanho (){
  geraSenha++;
 }
 
-const CampoSenha = document.querySelector("#campo-senha");
 
-const letraMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letraMinusculas = 'abcdefghijklmnopqrstuvwxyz';
-const numeros = '0123456789'
-const simbolos = '!@%*?';
+for (i=0;1< checkbox.length;i++){
+    checkbox[i].onclick = geraSenha;
+}
 
 function geraSenha(){
   let alfabeto = '';
@@ -49,10 +54,22 @@ function geraSenha(){
   }
   let senha ='';
   for(let i = 0; i <tamanhoSenha; i++{ 
-   let numeroALeatorio = Math.random() * letraMaiuscula.length;
+   let numeroALeatorio = Math.random() *alfabeto.length;
    numeroALeatorio = math.floor (numeroALeatorio);
    senha = senha + alfabeto[numeroALeatorio];
   )
 }
 campoSenha.value = senha;
+}
+
+
+function classficaSenha(){
+  forcaSenha.classList.remove('fraca,'media', forte');
+  if(tamanhoSenha>11){
+    forcaSenha.classList.add('forte');
+  }else if (tamanhoSenha > 5 && tamanhoSenha< 12){
+    forcaSenha.classList.add('media');
+  }else if (tamanhoSenha <= 5){
+    forcaSenha.classList.add('fraca');
+  }
 }
