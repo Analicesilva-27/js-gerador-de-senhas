bc65a4587ab2d4aeb2d99296edb342008db979ba
Analicesilva-27 <da.silva.analice@escola.pr.gov.br>
2025-09-19 07:46:14 -0300

    valor entropia

--- main.js
+++ main.js
@@ -63,13 +63,17 @@ campoSenha.value = senha;
 }
 
 
-function classficaSenha(){
+function classficaSenha(tamanhoAlfabeto){
+  let entropia = tamanhoSenha* Math.log2(tamanhoAlfabeto);
+  console.log(entropia);
   forcaSenha.classList.remove('fraca,'media', forte');
-  if(tamanhoSenha>11){
+  if(entropia>57){
     forcaSenha.classList.add('forte');
-  }else if (tamanhoSenha > 5 && tamanhoSenha< 12){
+  }else if (tamanhoSenha > 35 && entropia<57){
     forcaSenha.classList.add('media');
-  }else if (tamanhoSenha <= 5){
+  }else if (tamanhoSenha <= 35){
     forcaSenha.classList.add('fraca');
   }
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
 }
